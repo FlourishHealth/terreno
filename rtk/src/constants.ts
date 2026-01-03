0af1983f021e589cb6165b08c4c48d20ea9c269d
Josh Gachnang <josh@nang.io>
2026-01-03 16:59:30 -0600

    Update rtk publish to wait on ui

--- rtk/src/constants.ts
+++ rtk/src/constants.ts
@@ -16,18 +16,7 @@ export const logAuth = (...args: string[]): void => {
   }
 };
 
-// Handy debug logging for form presence, but not enabled by default.
-const FORM_PRESENCE_DEBUG = Constants.expoConfig?.extra?.FORM_PRESENCE_DEBUG === "true";
-if (FORM_PRESENCE_DEBUG) {
-  console.info("FORM_PRESENCE_DEBUG is enabled");
-}
-export const logFormPresence = (...args: string[]): void => {
-  if (FORM_PRESENCE_DEBUG) {
-    console.debug(...args);
-  }
-};
-
-// Handy debug logging for form presence or other socket events, but not enabled by default.
+// Handy debug logging socket events, but not enabled by default.
 // Can also be enabled by user feature flag.
 const WEBSOCKETS_DEBUG = Constants.expoConfig?.extra?.WEBSOCKETS_DEBUG === "true";
 if (WEBSOCKETS_DEBUG) {
@@ -43,15 +32,6 @@ export const logSocket = (
     console.debug(`[websocket]`, ...args);
   }
 };
-
-// Emit a focus event every 5 seconds while the question is focused.
-export const FORM_PRESENCE_INTERVAL_MS = 5000;
-// If we haven't received a focus event for the past 15 seconds, consider the question blurred.
-// The user may have navigated away from the question.
-export const FORM_PRESENCE_BLUR_TIMEOUT_MS = 15000;
-// Ensure the blur from the previous question has been sent before emitting the focus for this
-// question
-export const FORM_PRESENCE_DELAY_MS = 200;
 
 // When we use "expo publish", we want to point the API at the prod API. In the future,
 // we'll want to point at the staging API, and probably have a development release channel.
