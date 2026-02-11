8275da21fbf6531e7bcb955c617e60ffc7d3e702
Freddie Cuenca <freddie@flourish.health>
2026-02-11 14:21:12 -0700

    Lint fix

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,3 +1,5 @@
+// Re-export React Native style types for use in consumer projects
+export type {StyleProp, ViewStyle} from "react-native";
 export * from "./Accordion";
 export * from "./ActionSheet";
 export * from "./AddressField";
@@ -87,9 +89,6 @@ export * from "./UserInactivity";
 export * from "./Utilities";
 export * from "./useStoredState";
 export * from "./WebAddressAutocomplete";
-
-// Re-export React Native style types for use in consumer projects
-export type {StyleProp, ViewStyle} from "react-native";
 
 // export * from "./Layout";
 // export * from "./Drawer";
