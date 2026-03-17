e57f8e1e76f108170b18c70358e7a84ead688ba2
Josh Gachnang <josh@nang.io>
2026-03-17 14:18:32 -0500

    Add page wrapper to consents screen

--- example-frontend/app/(tabs)/consents.tsx
+++ example-frontend/app/(tabs)/consents.tsx
@@ -1,9 +1,13 @@
-import {ConsentHistory} from "@terreno/ui";
+import {ConsentHistory, Page} from "@terreno/ui";
 import type React from "react";
 import {terrenoApi} from "@/store/sdk";
 
 const ConsentsScreen: React.FC = () => {
-  return <ConsentHistory api={terrenoApi} />;
+  return (
+    <Page title="My Consents">
+      <ConsentHistory api={terrenoApi} />
+    </Page>
+  );
 };
 
 export default ConsentsScreen;
